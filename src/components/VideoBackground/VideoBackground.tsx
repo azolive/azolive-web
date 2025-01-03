3f2a94c7b76fe3a471db4e6f5cb923b73064984f
Bao Vien Ngo <bao@vi1.no>
2025-01-04 00:31:02 +0100

    Autoplay video on mobile devices

--- src/components/VideoBackground/VideoBackground.tsx
+++ src/components/VideoBackground/VideoBackground.tsx
@@ -1,12 +1,13 @@
 import "./VideoBackground.css";
-import backgroundVideo from "../../assets/bg.webm";
+import bgMp4 from "../../assets/bg.mp4";
+import bgWemp from "../../assets/bg.webm";
 
 export const VideoBackground = () => {
-  console.log(backgroundVideo);
   return (
     <div className="video-container">
-      <video className="background-video" autoPlay loop muted>
-        <source src={backgroundVideo} type="video/mp4" />
+      <video className="background-video" playsInline={true} autoPlay={true} muted={true} loop={true}>
+        <source src={bgMp4} type="video/mp4" />
+        <source src={bgWemp} type="video/webm" />
         Your browser does not support the video tag.
       </video>
     </div>
